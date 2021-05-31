79dc4797160d703f3f0985803da719b7c391df75
AnthonyLamour <a.lamour@ludus-academie.com>
2021-05-31 11:50:41 +0200

    Ajout set du pawn de base

--- ToonTanksUE/Intermediate/Build/Win64/UE4Editor/Inc/ToonTanks/ToonTanks.init.gen.cpp
+++ ToonTanksUE/Intermediate/Build/Win64/UE4Editor/Inc/ToonTanks/ToonTanks.init.gen.cpp
@@ -21,8 +21,8 @@ void EmptyLinkFunctionForGeneratedCodeToonTanks_init() {}
 				nullptr,
 				0,
 				PKG_CompiledIn | 0x00000000,
-				0x28447524,
-				0x75FC1632,
+				0x5835B5EB,
+				0x6A6817A0,
 				METADATA_PARAMS(nullptr, 0)
 			};
 			UE4CodeGen_Private::ConstructUPackage(ReturnPackage, PackageParams);
